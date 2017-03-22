e5e28693517610d5d8d6fbee4271c68725fff5e1
Adrien Zaganelli <adrienzaganelli@gmail.com>
2017-03-22 02:11:07 +0100

    :package: improve webpack outputs

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,5 +3,6 @@ import DomManipulator from './models/class.dom-manipulator';
 
 const $ = new DomManipulator();
 
-console.log('let\'s code !', detector);
+console.log('Elle est bonne !');
+console.info(detector);
 console.debug('move your ', $.el('body'));
