26d2a912f9a112b7a2d8052a11ed8d290aa20a59
AdrienZ <adrienzaganelli@gmail.com>
2018-04-06 00:08:53 +0200

    run prettier on codebase

--- app/src/js/modules/class.dom-manipulator.js
+++ app/src/js/modules/class.dom-manipulator.js
@@ -1,27 +1,26 @@
 /** ======================================================================= *
-* @desc jQuery $ alternative in native js                                 *
-* @param node selector | :string                                          *
-* @return HTMLnodeElement                                                 *
-* ======================================================================= *
-**/
+ * @desc jQuery $ alternative in native js                                 *
+ * @param node selector | :string                                          *
+ * @return HTMLnodeElement                                                 *
+ * ======================================================================= *
+ **/
 
-/* eslint-disable */
 class DomManipulator {
   id(id) {
-      return document.getElementById(id);
+    return document.getElementById(id)
   }
   el(el) {
-      return document.querySelector(el);
+    return document.querySelector(el)
   }
-  class (elClass) {
-      return document.getElementsByClassName(elClass);
+  class(elClass) {
+    return document.getElementsByClassName(elClass)
   }
   tag(tag) {
-      return document.getElementsByTagName(tag);
+    return document.getElementsByTagName(tag)
   }
   all(els) {
-      return document.querySelectorAll(els);
+    return document.querySelectorAll(els)
   }
 }
 
-module.exports = DomManipulator;
+module.exports = DomManipulator
