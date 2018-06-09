39798e16f833952eae8421b01b1593febcae1f2e
AdrienZ <adrienzaganelli@gmail.com>
2018-06-10 00:37:04 +0200

    add some comments for devserver and entries

--- config/devserver.js
+++ config/devserver.js
@@ -1,8 +1,19 @@
-// =======================================================================//
-// !  CONFIG DEV SERVER                                                   //
-// =======================================================================//
 const ip = require('ip')
 const env = require('./env')
+
+/*
+// =======================================================================//
+//                                                                        //
+// !  DEV SERVER CONFIG                                                   //
+//                                                                        //
+// *  native webpack-dev-server options with some env variables           //
+// *  .ejs files in subfolders can still be use without getting complied  //
+// *  url related option can be set in the .env file                      //
+//                                                                        //
+// ?  https://webpack.js.org/configuration/dev-server/                    //
+//                                                                        //
+// =======================================================================//
+*/
 
 const serverIp = ip.address()
 const serverPort = env.DEV_SERVER_PORT
@@ -11,14 +22,14 @@ const serverUseLocalIp = env.DEV_SERVER_LOCAL_IP === 'true'
 const serverHost = serverUseLocalIp ? serverIp : null
 
 module.exports = {
+  // i will never know how to use this ⬇️
   // contentBase: urls.dev.root,
-  // change this as you want
-  compress: true, //gzip
+  compress: true, // gzip
   inline: true,
   noInfo: true,
   before: () => {
     const s = serverHttps ? 's' : ''
-    console.log(
+    console.info(
       'Project is running at http' +
         s +
         '://' +
@@ -34,7 +45,7 @@ module.exports = {
   },
   clientLogLevel: 'error',
   historyApiFallback: true,
-  hot: true,
+  hot: env.DEV_SERVER_HMR,
   https: serverHttps,
   open: false,
   progress: false,
--- config/entries.js
+++ config/entries.js
@@ -1,20 +1,31 @@
 const fs = require('fs')
 const urls = require('./urls')
 
+/*
 // =======================================================================//
-// !  CONFIG ENTRIES / SCRIPTS / BUNDLES                                  //
+//                                                                        //
+// !  JAVASCRIPT ENTRIES                                                  //
+//                                                                        //
+// *  2 modes: one entry main.js or multi entries: { multi: true }        //
+// *  in multi mode, return all the js files at the root of app/js        //
+// *  🚨 WARNING: by default all entries will be injected by the          //
+// *  🚨 html-webpack-plugin, use excludeChunks option                    //
+//                                                                        //
+// * this will be the entry of our webpack configuration                  //
+//                                                                        //
 // =======================================================================//
+*/
+
 const scripts = (options = {}) => {
   const jsPath = urls.aliases['@js']
 
   if (options.multi) {
     let SCRIPTS = {}
-    fs
-      .readdirSync(jsPath)
+    fs.readdirSync(jsPath)
       .filter(file => file.match(/.js/))
       .map(path => {
-        // all these files are now entries
         const bundle_name = path.replace('.js', '')
+        // add suffix _bundle to script name
         SCRIPTS[`${bundle_name}_bundle`] = [`${jsPath}/${path}`]
       })
     return SCRIPTS
@@ -23,7 +34,22 @@ const scripts = (options = {}) => {
   return { main_bundle: [`${jsPath}/main`] }
 }
 
-// List all files in a directory in Node.js recursively in a synchronous fashion
+/*
+// =======================================================================//
+//                                                                        //
+// !  IMAGES ENTRIES                                                      //
+//                                                                        //
+// *  List all files in a dir via Node.js recursively and synchronously   //
+// *  🙈 importing your images in js so webpack detect it is painfull     //
+// * plus it had lot of complexity compared to a backend                  //
+// * or url often break between webpack-dev-server and prod               //
+// * because of the ✨ url-checking feature from css-loader               //
+//                                                                        //
+// * this will be added to the entries of our webpack configuration       //
+//                                                                        //
+// =======================================================================//
+*/
+
 const walkSync = (dir, filelist = {}) => {
   const files = fs.readdirSync(dir)
   let resfilelist = filelist
@@ -38,12 +64,19 @@ const walkSync = (dir, filelist = {}) => {
   })
   return resfilelist
 }
-
 const imgs = () => walkSync(urls.aliases['@img'])
 
+/*
 // =======================================================================//
-// !  CONFIG VIEWS / HTML                                                 //
+//                                                                        //
+// !  VIEWS ENTRIES                                                       //
+//                                                                        //
+// *  turn .ejs files at the root of /app/ into html files                //
+// *  .ejs files in subfolders can still be use without getting complied  //
+//                                                                        //
 // =======================================================================//
+*/
+
 const views = () =>
   fs.readdirSync(urls.dev.root).filter(file => file.match(/.ejs$/))
 
