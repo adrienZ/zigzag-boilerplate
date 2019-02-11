a6577be470949428dceab9183b425b2d48a58079
AdrienZ <adrienzaganelli@gmail.com>
2019-02-11 02:25:29 +0100

    reorder main

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,15 +1,21 @@
 import '@sass/style.scss'
 import '@js/polyfills'
 
+console.info('elle est bonne')
+
 /**
+ * ASYNC MODULES
+ *
  * here is an example of asynchronious modules.
  * async imports are simple promise, it allow us to
  * reduce the size of our inital javascript bundle.
+ *
+ * arugments: webpackPrefetch(bool), webpackPreload(bool), webpackChunkName(string)
+ *
+ * see more: https://webpack.js.org/guides/code-splitting/
  */
 
 const ModuleAsyncImportPromise = import(/* webpackChunkName: "ip-async-test" */ 'ip')
 ModuleAsyncImportPromise.then(module => {
   console.log('async import works !', module.address(), module)
 })
-
-console.info('js goes here')
