653120eab21951ec7b76f2b1fae8910b8c5dbb2f
AdrienZ <adrienzaganelli@gmail.com>
2019-02-11 01:27:47 +0100

    remove navigo, prefer abstact class

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,15 +1,4 @@
 import '@sass/style.scss'
-import '@js/features/serviceWorkerRuntime'
+// import '@js/features/serviceWorkerRuntime'
 
-import Navigo from 'navigo'
-import HomePage from '@page/Home'
-
-const root = null
-const useHash = true // Defaults to: false
-const hash = '#!' // Defaults to: '#'
-const router = new Navigo(root, useHash, hash)
-
-router.on('/', HomePage).resolve()
-
-// 404 script
-router.notFound(() => {})
+console.info('js goes here')
