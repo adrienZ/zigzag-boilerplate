e389e1a449057470e4d412fcb0fc0495842e51db
AdrienZ <adrienzaganelli@gmail.com>
2020-07-11 14:01:41 +0200

    css vars

--- app/src/js/polyfills.js
+++ app/src/js/polyfills.js
@@ -1,3 +1,7 @@
 // https://babeljs.io/docs/en/babel-plugin-syntax-dynamic-import#working-with-webpack-and-babel-preset-env
+// support promise
 import 'core-js/modules/es6.promise'
 import 'core-js/modules/es6.array.iterator'
+
+// support css variables
+import 'ie11-custom-properties'
